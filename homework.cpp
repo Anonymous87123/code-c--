3d8b9a915df6b4a055579ad4cc7f8288f874cf0a
Anonymous87123 <haizaigahei@outlook.com>
2025-10-01 13:54:50 +0800

    dew

--- homework.cpp
+++ homework.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 int main()
 {
-    int m1;
-    int array[5];
-    while(cin>>m1)
-    {
-        array[0]=m1/10000;
-        int m2 = m1%10000;
-        array[1]=m2/1000;
-        int m3 = m2%1000;
-        array[2]=m3/100;
-        int m4 = m3%100;
-        array[3]=m4/10;
-        int m5 = m4%10;
-        array[4]=m5;
-        for(int i = 0; i < 5-i; i++)
-        {
-            int temp = array[i];
-            array[i] = array[4-i];
-            array[4-i] = temp;
-        }
-    cout<<array[0]<<"  "<<array[1]<<"  "<<array[2]<<"  "<<array[3]<<"  "<<array[4]<<endl;
+    int count = 0;
+    const int color = 5;
+    for (int c1 = 1; c1 <= color; c1++)
+    for (int c2 = 1; c2 <= color; c2++)
+    for (int c3 = 1; c3 <= color; c3++)
+    for (int c4 = 1; c4 <= color; c4++)
+    for (int c5 = 1; c5 <= color; c5++)
+    for (int c6 = 1; c6 <= color; c6++)
+    for (int c7 = 1; c7 <= color; c7++)
+    for (int c8 = 1; c8 <= color; c8++){
+        if (c1 != c2 && c1 != c4 && c1 != c5 &&
+            c2 != c1 && c2 != c3 && c2 != c6 &&
+            c3 != c2 && c3 != c4 && c3 != c7 &&
+            c4 != c1 && c4 != c3 && c4 != c8 &&
+            c5 != c1 && c5 != c6 && c5 != c8 &&
+            c6 != c5 && c6 != c2 && c6 != c7 &&
+            c7 != c6 && c7 != c3 && c7 != c8 &&
+            c8 != c7 && c8 != c4 && c8 != c5)
+        {count++;}
     }
+    cout << count << endl;
 }
